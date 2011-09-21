d4205f8ca79308981f0f7a12ecbb7a388c87db68
hendryluk <none@none>
2011-09-21 18:24:48 +1000

    Fixed tplHelper.Delay method

--- SheepJax/AsyncHelpers/SynchronisationHelper.cs
+++ SheepJax/AsyncHelpers/SynchronisationHelper.cs
@@ -8,29 +8,19 @@ namespace SheepJax.AsyncHelpers
     {
          public static IDisposable BeginReadLock(this ReaderWriterLockSlim lck)
          {
-             if (!lck.TryEnterReadLock(2000))
-                 throw new Exception("Blah!!");
-
-
-             //lck.EnterReadLock();
+             lck.EnterReadLock();
              return Disposable.Create(lck.ExitReadLock);
          }
 
          public static IDisposable BeginUpgradeableReadLock(this ReaderWriterLockSlim lck)
          {
-             if (!lck.TryEnterUpgradeableReadLock(2000))
-                 throw new Exception("Blah!!");
-
-             //lck.EnterUpgradeableReadLock();
+             lck.EnterUpgradeableReadLock();
              return Disposable.Create(lck.ExitUpgradeableReadLock);
          }
 
          public static IDisposable BeginWriteLock(this ReaderWriterLockSlim lck)
          {
-             if (!lck.TryEnterWriteLock(2000))
-                 throw new Exception("Blah!!");
-
-             //lck.EnterWriteLock();
+             lck.EnterWriteLock();
              return Disposable.Create(lck.ExitWriteLock);
          }
     }
